f3ba565f7da689ea0bc7aa439c9c1f0fdb318728
Sam Critchley <critchley.sj@gmail.com>
2016-05-20 23:03:03 +1000

    fixed some grevious errors, fixed tests, changed how search model is bound to a scope and also made it optional to bind to a scope

--- src/ng-quick-grid/src/search-model-factory.spec.js
+++ src/ng-quick-grid/src/search-model-factory.spec.js
@@ -9,7 +9,7 @@
     beforeEach(inject(function (SearchModel, $rootScope) {
       scope = $rootScope.$new();
       model = {
-        search: new SearchModel('Name desc'),
+        search: new SearchModel(null, 'Name desc'),
         results: null
       };
     }));
@@ -20,12 +20,9 @@
 
     it('should trigger a query when the model changes', function (done) {
       inject(function ($rootScope, $compile, $q) {
-        scope.model = model;
         var q = $q.defer();
         q.reject('test');
         var weAreDone = false;
-        var element = angular.element('<table quick-grid grid-model="model.search"></table>');
-        element = $compile(element)(scope);
 
         function search(obj) {
           if (obj.model.name === 'Foo' && !weAreDone) {
@@ -36,7 +33,8 @@
           return q.promise;
         }
 
-        model.search.attachToScope(scope, search);
+        model.search.callback = search;
+        model.search.watch(scope);
         scope.$digest();
         model.search.model.name = 'Foo';
         scope.$digest();
@@ -46,13 +44,9 @@
     it('should trigger a query when the filters change', function (done) {
       inject(function ($rootScope, $compile, $q) {
         /* jshint sub: true */
-        scope.model = model;
         var q = $q.defer();
         q.reject('test');
         var weAreDone = false;
-
-        var element = angular.element('<table quick-grid grid-model="model.search"></table>');
-        element = $compile(element)(scope);
 
         function search(obj) {
           var filterSet = obj.filters && obj.filters.Name && obj.filters.Name.value === 'Foo';
@@ -65,7 +59,8 @@
         }
 
         model.search.addFilter('Name', '%', 'Fred');
-        model.search.attachToScope(scope, search);
+        model.search.callback = search;
+        model.search.watch(scope);
         scope.$digest();
         expect(model.search.filters['Name'].value).toBe('Fred');
         model.search.filters['Name'].value = 'Foo';
@@ -76,16 +71,12 @@
     it('should trigger a query when the sorting changes', function (done) {
       inject(function ($rootScope, $compile, $q) {
         /* jshint sub: true */
-        scope.model = model;
         var q = $q.defer();
         q.reject('test');
         var weAreDone = false;
 
-        var element = angular.element('<table quick-grid grid-model="model.search"></table>');
-        element = $compile(element)(scope);
-
-        function search(obj, paging) {
-          var sortSet = paging.sortBy === 'Name';
+        function search(obj) {
+          var sortSet = obj.paging.sortBy === 'Name';
           if (sortSet && !weAreDone) {
             weAreDone = true;
             done();
@@ -94,7 +85,8 @@
           return q.promise;
         }
 
-        model.search.attachToScope(scope, search);
+        model.search.callback = search;
+        model.search.watch(scope);
         scope.$digest();
         model.search.paging.sort('Name');
         scope.$digest();
@@ -104,16 +96,12 @@
     it('should add \'desc\' to the sort when the same sort is run twice', function (done) {
       inject(function ($rootScope, $compile, $q) {
         /* jshint sub: true */
-        scope.model = model;
         var q = $q.defer();
         q.reject('test');
         var weAreDone = false;
 
-        var element = angular.element('<table quick-grid grid-model="model.search"></table>');
-        element = $compile(element)(scope);
-
-        function search(obj, paging) {
-          var sortSet = paging.sortBy === 'Foo desc';
+        function search(obj) {
+          var sortSet = obj.paging.sortBy === 'Foo desc';
           if (sortSet && !weAreDone) {
             weAreDone = true;
             done();
@@ -122,7 +110,8 @@
           return q.promise;
         }
 
-        model.search.attachToScope(scope, search);
+        model.search.callback = search;
+        model.search.watch(scope);
         scope.$digest();
         model.search.paging.sort('Foo');
         model.search.paging.sort('Foo');
@@ -133,16 +122,12 @@
     it('should set sort of undefined when sortby is null', function (done) {
       inject(function ($rootScope, $compile, $q) {
         /* jshint sub: true */
-        scope.model = model;
         var q = $q.defer();
         q.reject('test');
         var weAreDone = false;
 
-        var element = angular.element('<table quick-grid grid-model="model.search"></table>');
-        element = $compile(element)(scope);
-
-        function search(obj, paging) {
-          var sortSet = paging.sortBy === undefined;
+        function search(obj) {
+          var sortSet = obj.paging.sortBy === undefined;
           if (sortSet && !weAreDone) {
             weAreDone = true;
             done();
@@ -151,7 +136,8 @@
           return q.promise;
         }
 
-        model.search.attachToScope(scope, search);
+        model.search.callback = search;
+        model.search.watch(scope);
         scope.$digest();
         model.search.paging.sort(null);
         scope.$digest();
@@ -161,10 +147,7 @@
     it('should not change the pagecount when pagecount is null', function (done) {
       inject(function ($rootScope, $compile, $q) {
         /* jshint sub: true */
-        scope.model = model;
         var q = $q.defer();
-        var element = angular.element('<table quick-grid grid-model="model.search"></table>');
-        element = $compile(element)(scope);
 
         function search() {
           q.resolve({
@@ -175,7 +158,8 @@
         }
         model.search.pageCount.length = 27;
         model.search.addFilter('Name', '%', 'Fred');
-        model.search.attachToScope(scope, search);
+        model.search.callback = search;
+        model.search.watch(scope);
         scope.$digest();
         expect(model.search.filters['Name'].value).toBe('Fred');
         model.search.filters['Name'].value = 'Foo';
@@ -192,10 +176,7 @@
     it('should change the pagecount when pagecount is not value', function (done) {
       inject(function ($rootScope, $compile, $q) {
         /* jshint sub: true */
-        scope.model = model;
         var q = $q.defer();
-        var element = angular.element('<table quick-grid grid-model="model.search"></table>');
-        element = $compile(element)(scope);
 
         function search() {
           q.resolve({
@@ -205,7 +186,6 @@
 
           //hacky
           setTimeout(function () {
-            console.log(model.search.pageCount.length);
             expect(model.search.pageCount.length).toBe(10);
             done();
           }, 500);
@@ -214,7 +194,8 @@
 
         model.search.pageCount.length = 27;
         model.search.addFilter('Name', '%', 'Fred');
-        model.search.attachToScope(scope, search);
+        model.search.callback = search;
+        model.search.watch(scope);
         scope.$digest();
         expect(model.search.filters['Name'].value).toBe('Fred');
         model.search.filters['Name'].value = 'Foo';
@@ -228,7 +209,7 @@
         model.search.addFilter('Name', '%', 'Fred');
         var queryString = model.search.toQueryString();
 
-        expect(queryString).toBe('filters[0].key=Foo&filters[0].value=%3DTest&filters[2].key=Name&filters[2].value=%25Fred%25&paging.pageIndex=0&paging.sortBy=Name%20desc&paging.filterHash=');
+        expect(queryString).toBe('filters[0].key=Foo&filters[0].value=%3DTest&filters[1].key=Name&filters[1].value=%25Fred%25&paging.pageIndex=0&paging.sortBy=Name%20desc&paging.filterHash=');
       });
     });
 
