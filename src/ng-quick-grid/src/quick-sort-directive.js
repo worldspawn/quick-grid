d644fcc7311d5800a1f48476a471fddccfd6f33e
Sam Critchley <critchley.sj@gmail.com>
2016-09-28 17:03:39 +1000

    housekeeping

--- src/ng-quick-grid/src/quick-sort-directive.js
+++ src/ng-quick-grid/src/quick-sort-directive.js
@@ -6,8 +6,8 @@ function directive($compile) {
             var sortBy = $attrs.quickSort;
             var carets = angular.element('<span ng-if="grid.searchModel.paging.sortBy.toLowerCase().indexOf(\'' + sortBy.toLowerCase() + '\') === 0"><i class="glyphicon glyphicon-triangle-bottom" ng-if="grid.searchModel.paging.sortBy.toLowerCase().indexOf(\'desc\') > -1"></i><i class="glyphicon glyphicon-triangle-top" ng-if="grid.searchModel.paging.sortBy.toLowerCase().indexOf(\'desc\') === -1"></i></span>');
             $element.append($compile(carets)($scope));
-            $element.on('click', function() {
-                $scope.$apply(function() {
+            $element.on('click', () => {
+                $scope.$apply(() => {
                     quickGrid.searchModel.paging.sort(sortBy);
                 });
             });
