19647d3e1880184382da0f0c9ead39658bb9f434
Alastair Pitts <alastair@pitts.io>
2016-10-05 10:39:30 +1100

    Changed the QueryResults to now return a executed list This allows for greater control of when the query is executed

--- src/QuickGrid/QueryResult.cs
+++ src/QuickGrid/QueryResult.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
 namespace QuickGrid
@@ -24,15 +26,32 @@ namespace QuickGrid
         /// </summary>
         [JsonConverter(typeof(LazyConverter<int?>))]
         public Lazy<int?> Total { get; set; }
+
         /// <summary>
         /// Reading the page size will trigger a read of Total which *may* trigger a count query. A matching filterhash will cause the total to not be resolved.
         /// </summary>
         [JsonConverter(typeof(LazyConverter<int?>))]
         public Lazy<int?> PageCount { get; }
+
         public int PageIndex { get; set; }
 
         public int? PageSize { get; set; }
-        public IQueryable<T> Results { get; set; }
+
+        [JsonIgnore]
+        public IQueryable<T> ResultsQuery { get; set; }
+
+        public List<T> Results { get; set; }
+
         public string FilterHash { get; set; }
+
+        [OnSerializing]
+        internal void OnSerializing(StreamingContext context)
+        {
+            if (Results == null || ResultsQuery == null)
+                return;
+
+            //execute the results query
+            Results = ResultsQuery.ToList();
+        }
     }
 }
