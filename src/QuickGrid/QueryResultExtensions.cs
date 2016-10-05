009da7d95809618a3ac53609dfdbaab9b98d91a2
Alastair Pitts <alastair@pitts.io>
2016-10-05 11:53:07 +1100

    Added an extension method to allow a query result type to be changed

---
+++ src/QuickGrid/QueryResultExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace QuickGrid
+{
+    public static class QueryResultExtensions
+    {
+        public static QueryResult<TOut> Project<TIn, TOut>(this QueryResult<TIn> source, Expression<Func<TIn, TOut>> projection)
+        {
+            //excute the original query
+            var results = source.Results.ToList();
+
+            var newQuery = results.AsQueryable().Select(projection);
+
+            return new QueryResult<TOut>
+            {
+                PageIndex = source.PageIndex,
+                PageSize = source.PageSize,
+                Total = source.Total,
+                FilterHash = source.FilterHash,
+                Results = newQuery
+            };
+        }
+    }
+}
